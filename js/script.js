fde842d8cd0394d8147874340a63d03c4a462b5c
Travis Martin <travisemerymartin@gmail.com>
2018-12-24 20:29:55 +0000

    Done.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-  $("#text").append("OMG IT WORKED JESUS FACKING CHRIST THAT TOOK FOREVER");
+  $("#text").append("OMG IT WORKED JESUS FACKING CHRIST THAT TOOK FOREVER")
 });
