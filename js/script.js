ea9131900984f83dd21d53b335febe045b2a756a
masterful-array-3850 <crgammage@gmail.com>
2020-04-12 14:55:40 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script>
+$("#text").append("this is so freaking cool.");
+</script>
+
+$(document).ready(function() {
+    // code to be executed goes here
+});
